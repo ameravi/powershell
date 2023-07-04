bd2a1526e703b4ba88ca2db0577736b9a3b6b82a
ameravi <106415283+ameravi@users.noreply.github.com>
2023-07-04 00:16:13 -0400

    Update install-applications.ps1

--- install-applications.ps1
+++ install-applications.ps1
@@ -1,9 +1,9 @@
-## Google Chrome ###
+## Google Chrome Install ###
 
 $Path = $env:TEMP; $Installer = 'chrome_installer.exe'; Invoke-WebRequest -Uri 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe' -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args '/silent /install' -Verb RunAs -Wait; Remove-Item -Path $Path\$Installer
-Write-Output "Google Chrome Enterprise is installed1"
+Write-Output "Google Chrome Enterprise is installed!"
 
-### Notepad++ ###
+### Notepad++ Install###
 
 $path = $env:TEMP
 $NPPURI = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.4/npp.8.5.4.Installer.x64.exe'
@@ -12,5 +12,6 @@ $NPPInstaller = npp.8.5.4.Installer.x64.exe
 Invoke-WebRequest -Uri $NPPURI -Outfile $Path\$Installer
 Start-Process -FilePath $Path\$NPPInstaller -Args '/S /NCRC'
 Write-Output "Notepad ++ Is installed!"
+
 
 
