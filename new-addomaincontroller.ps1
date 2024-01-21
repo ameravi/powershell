8b751ae6c5bb46c8253d50b39c4d88316fe59388
ameravi <106415283+ameravi@users.noreply.github.com>
2024-01-21 15:14:49 -0500

    Create new-addomaincontroller.ps1

---
+++ new-addomaincontroller.ps1
@@ -0,0 +1,9 @@
+$DomainName = 'local.meravi.us
+
+## Upgrade the server to a domain controller
+
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+## Configure the server as a domain controller.  This command will prompt for a dsrm password and ask to confirm the install, then reboot
+
+Install-ADDSDomainController -InstallDns -DomainName "local.meravi.us" -SafeModeAdministratorPassword
