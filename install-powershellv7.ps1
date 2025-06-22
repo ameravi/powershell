5c59cb8fb701c075459e739cd6a35e18f54f110a
ameravi <106415283+ameravi@users.noreply.github.com>
2025-06-22 19:43:04 -0400

    Create install-powershellv7.ps1
    Splitting this off to its own script

---
+++ install-powershellv7.ps1
@@ -0,0 +1,15 @@
+##Installs powershell 7
+
+function install-powershell7 {
+#This script installs Powershell 7.5.2 from a powershell 5.1 shell
+
+$psuri = 'https://github.com/PowerShell/Powershell/releases/download/v7.5.0/Powershell-7.5.2-win-x64.msi'
+$installer = 'Powershell-7.5.0-win-x64.msi'
+$path = $env:TEMP
+$ps7path = 'c:\program files\powershell\7\pwsh.exe'
+
+invoke-webrequest -URI $psuri -OutFile $Path\$Installer
+msiexec /i $path\$installer /qn
+}
+
+install-powershell7
