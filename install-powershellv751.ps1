3e150b20ff9a69dc8d956471d82b496962927561
ameravi <106415283+ameravi@users.noreply.github.com>
2025-06-27 10:18:08 -0400

    Update and rename install-powershellv7.ps1 to install-powershellv751.ps1

--- install-powershellv751.ps1
+++ install-powershellv751.ps1
@@ -1,10 +1,10 @@
 ##Installs powershell 7
 
 function install-powershell7 {
-#This script installs Powershell 7.5.2 from a powershell 5.1 shell
+#This script installs Powershell 7.5.1 from a powershell 5.1 shell
 
-$psuri = 'https://github.com/PowerShell/Powershell/releases/download/v7.5.0/Powershell-7.5.2-win-x64.msi'
-$installer = 'Powershell-7.5.0-win-x64.msi'
+$psuri = 'https://github.com/PowerShell/Powershell/releases/download/v7.5.1/Powershell-7.5.1-win-x64.msi'
+$installer = 'Powershell-7.5.1-win-x64.msi'
 $path = $env:TEMP
 $ps7path = 'c:\program files\powershell\7\pwsh.exe'
 
