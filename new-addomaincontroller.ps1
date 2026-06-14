3dd01f57ec58b3599875beaf46f9982e4b9bca43
ameravi <106415283+ameravi@users.noreply.github.com>
2026-06-14 13:06:15 -0400

    Update SafeModeAdministratorPassword to use SecureString

--- new-addomaincontroller.ps1
+++ new-addomaincontroller.ps1
@@ -8,4 +8,4 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 ## Configure the server as a domain controller.  This command will prompt for a dsrm password and ask to confirm the install, then reboot
 
-Install-ADDSDomainController -InstallDns -DomainName $DomainName -SafeModeAdministratorPassword $SafeModeAdministratorPassword
+Install-ADDSDomainController -InstallDns -DomainName $DomainName -SafeModeAdministratorPassword $SecureString
